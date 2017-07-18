7b5bb1b14b232b36a6fda1e439f7a15466e4c281
Johnny Watson <johnny@streetchange.org.uk>
2017-07-18 13:51:34 +0100

    Created rails project, with team model and seeds

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.firs
+
+Team.delete_all
+
+team1 = Team.create({name: "Gryffindor"}) 
+team2 = Team.create({name: "Slytherin"}) 
+team3 = Team.create({name: "Hufflepuff"}) 
+team4 = Team.create({name: "Ravenclaw"}) 
